52905398e6fe268fe68adb05168a98e212448c9d
Richard Tran <richardtran@Richards-MacBook-Air-2.local>
2023-02-23 13:14:12 -0600

    adde surface energy calculator

--- analysis/surface_energy.py
+++ analysis/surface_energy.py
@@ -1,1 +1,19 @@
-print()
+from pymatgen.analysis.surface_analysis import SlabEntry
+from pymatgen.entries.computed_entries import ComputedEntry
+from pymatgen.io.ase import AseAtomsAdaptor
+
+def get_slab_entry(dat):
+
+    atoms=Atoms(dat.atomic_numbers,
+                positions=dat.pos,
+                tags=dat.tags,
+                cell=dat.cell.squeeze(), pbc=True)
+    
+    return SlabEntry(AseAtomsAdaptor.get_structure(atoms), dat.y, dat.miller)
+
+def get_surface_energy(dat):
+    bulk_entry = ComputedEntry(dat.bulk_formula, dat.bulk_energy)
+    gas_entry = ComputedEntry('O2', 2*-7.204) # the ref energy for O in OC20
+    slabentry = get_slab_entry(dat)
+    
+    return slabentry.surface_energy(bulk_entry, [gas_entry])
