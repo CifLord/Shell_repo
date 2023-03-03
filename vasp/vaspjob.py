aa6a6d8d42255d317f89549c84161a17bda35f19
Richard Tran <richardtran@Richards-MacBook-Air-2.local>
2023-03-02 19:33:37 -0600

    bug

--- vasp/vaspjob.py
+++ vasp/vaspjob.py
@@ -78,8 +78,8 @@ if __name__ == "__main__":
             'Mn3+': 4, 'Mn4+': 3, 'Mo': 5, 'Nd3+': 3, 'Ni': 5, 'Pm3+': 4, 
             'Pr3+': 2, 'Sm3+': 5, 'Tb3+': 6, 'Tm3+': 2, 'V': 5, 'W': 5, 'Yb3+': 1}   
     
-    vasp_params['magmom'] = [mags['INCAR']['MAGMOM'][site.symbol] if site.symbol in \
-                             mags['INCAR']['MAGMOM'].keys() else 0.6 for site in atoms]
+    vasp_params['magmom'] = [mags[site.symbol] if site.symbol in \
+                             mags.keys() else 0.6 for site in atoms]
     
     # Run VASP
     calc = Vasp(**vasp_params)
