a83295a0cf7fcf10a39c6cbd49f8169807299af7
moxx <moxx@gmail.com>
2023-05-09 21:57:28 +0000

    1. Add candidates to run predictions

--- calculators/bash_run.sh
+++ calculators/bash_run.sh
@@ -2,8 +2,8 @@
 
 echo starting 
 
-CUDA_VISIBLE_DEVICES=2 python run_predictions.py -m "mp-505009 " -i "prediction/phase1.lmdb" &
-CUDA_VISIBLE_DEVICES=3 python run_predictions.py -m "candidates.json" -i "prediction/phase2.lmdb" &
+CUDA_VISIBLE_DEVICES=2 python run_predictions.py -m "Candidates/dgx_1.json " -i "prediction/phase1.lmdb" &
+CUDA_VISIBLE_DEVICES=3 python run_predictions.py -m "Candidates/dgx_2.json" -i "prediction/phase2.lmdb" &
 
 
 echo finished
