2d0885ae917fbb9c1f74e829b4f1a3fbaebbebae
moxx <moxx@gmail.com>
2023-07-25 11:44:52 +0000

    debug

--- calculators/run_continue1.py
+++ calculators/run_continue1.py
@@ -7,33 +7,29 @@ sys.path.append(repo_dir)
 import json
 import argparse
 
-
 def read_options():
-
     parser = argparse.ArgumentParser()
-
     parser.add_argument("-i", "--traj_dir", dest="traj_dir", type=str, 
                         help="Location of trajs")
-
     parser.add_argument("-d", "--gpus", dest="gpus", type=int, default=4,
-                        help="Number of gpus to distribute jobs across")
-
-
+                        help="Number of GPUs to distribute jobs across")
     args = parser.parse_args()
-
     return args
 
-if __name__=="__main__":
-
-    args = read_options() 
-
+if __name__ == "__main__":
+    args = read_options()
     traj_in = args.traj_dir
     num_gpus = args.gpus
 
     traj_paths = [os.path.join(args.traj_dir, f) for f in os.listdir(args.traj_dir)]
-    num_paths=len(traj_paths)//num_gpus
-    os.makedirs('./Candidates/traj_paths',exist_ok=True)
-    for i in range(num_gpus):
-        with open(f'Candidates/traj_paths/trajs{i}.json', 'w') as f:
-            json.dump(traj_paths[i:(i+1)*num_paths], f)
+    num_paths_per_gpu = len(traj_paths) // num_gpus
+    remainder_paths = len(traj_paths) % num_gpus
 
+    os.makedirs('./Candidates/traj_paths', exist_ok=True)
+
+    start_idx = 0
+    for i in range(num_gpus):
+        end_idx = start_idx + num_paths_per_gpu + (1 if i < remainder_paths else 0)
+        with open(f'Candidates/traj_paths/trajs{i}.json', 'w') as f:
+            json.dump(traj_paths[start_idx:end_idx], f)
+        start_idx = end_idx
