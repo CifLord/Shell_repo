34a35e77ed85352ed79a778245ad934dc650a98f
CifLord <rit634989@gmail.com>
2023-08-04 17:33:00 -0500

    added function to analysis change in overpotentail as a Pourbaix diagram

--- analysis/surface_energy.py
+++ analysis/surface_energy.py
@@ -352,92 +352,35 @@ def plot_P_vs_T(list_of_dat, T_range, lnP_range, increment=100):
     
     return plt
 
-
-def get_surface_pbx_facets(queryengine, criteria, T=298.15):
-
-    entries = list(queryengine.get_slab_entries(criteria).values())
-    ref_entries = {}
-    for entry in entries:
-        bulk_entry = ComputedStructureEntry.from_dict(bulk_oxides_dict[entry.data['mpid']])
-        if entry.data['mpid'] not in ref_entries:
-            ref_entries[entry.data['mpid']] = get_ref_entries(bulk_entry, 
-                                                               MAPIKEY=queryengine.MAPIKEY)
-        preset_slabentry_se(entry, bulk_entry, MAPIKEY=queryengine.MAPIKEY,
-                            ref_entries=ref_entries[entry.data['mpid']])
-        
-    slabentry_dict, pbx_line_dict = {}, {}
-    for slabentry in entries:
-        mpid = slabentry.data['mpid']
-        if mpid not in slabentry_dict.keys():
-            slabentry_dict[mpid] = {}
-            pbx_line_dict[mpid] = {}
-        hkl = slabentry.miller_index
-        if hkl not in slabentry_dict[mpid].keys():
-            slabentry_dict[mpid][hkl] = []
-            pbx_line_dict[mpid][hkl] = {}
-        slabentry_dict[mpid][hkl].append(slabentry)
-    
-    """
-    The surface energy of nonstoichometric oxide surface as a fucntion of pH and O 
-    (e.g. surface grand potential) is used to determine the Pourbaix diagram. It is 
-    given as γ = (E^* - Σnμ_i)/2A. Since we're building a Pourbaix diagram on a per 
-    facet basis, we can just ignore 2A for now since all facets we are considering 
-    have the same surface area. μ_i can be rewritten as the chemical potential of 
-    oxygen only, e.g. μ_O. Here, we assume the following chemical reaction takes 
-    place under an aqueous condition: μ_H2O(l) --> 2μ_H + μ_O. As such, 
-    μ_O = μ_H2O(l) - 2μ_H. We assume μ_H2O(l) = E_H2O (Gibbs free energy of H2O in 
-    a box). We then assume charge transfer when a H atom is in the reaction, e.g.:
-    μ_H = μ_H^+ + μ_e- = 1/2G_H2 - eU + kBTlna^+ via the Nernst Equation. Ergo
-    μ_O = G_H2O(l) - G_H2 + 2eU - 2kBTlna^+. Since we don't have G, we'll just add
-    a correction value for converting between E and G: 
-    μ_O = E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr
-    Lastly, we can also reference μ_O to the energy of O2 (keep it simple and 
-    reference to 1/2E_O2 for now) which gives us Δμ_O = μ_O-1/2EO_2 or:
-    Δμ_O = E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr - 1/2E_O2
-    As such, the surface grand potential can be written as:
-    2Aγ(U, pH) = E^* - n(E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr - 1/2E_O2) 
-    where n determines the number of excess/defficient species relative to oxygen.
-    """
-    muH2O = -14.231 # DFT energy per formula of H2O from MP 
-    muH2 = -6.7714828 # DFT energy per formula of H2 from MP 
-    EO = -4.946243415 # DFT energy per atom of O2 from MP
-    muO = muH2O - muH2 - 2*queryengine.get_e_transfer_corr(T, U=Symbol('U'), pH=Symbol('pH'))
-    
-    for mpid in slabentry_dict.keys():
-        bulk_formula = queryengine.surface_properties.distinct('bulk_reduced_formula',
-                                                               {'entry_id': mpid})[0]
-
-        for hkl in slabentry_dict[mpid].keys():
-            print(hkl)
-            stable_facet = []
-            
-            # get most stable amongst unique stoich.
-            stable_stoich_dict = {}
-            for slabentry in slabentry_dict[mpid][hkl]:
-                if slabentry.composition not in stable_stoich_dict.keys():
-                    stable_stoich_dict[slabentry.composition] = []
-                stable_stoich_dict[slabentry.composition].append(slabentry)
-            for comp in stable_stoich_dict.keys():
-                stable_stoich_dict[comp] = sorted(stable_stoich_dict[comp], key=lambda entry: entry.energy)
-            stable_slabentries = [stable_stoich_dict[c][0] for c in stable_stoich_dict.keys()]
-
-            # Solve system of two eqns for each pair of surfaces as U(pH)
-            for c in itertools.combinations(stable_slabentries, 2):
-                eqn1 = c[0].preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
-                eqn2 = c[1].preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
-                pbx_line_dict[mpid][hkl][c] = solve(eqn1 - eqn2, Symbol('U'))
-                print(solve(eqn1 - eqn2, Symbol('U')))
-                
-    return pbx_line_dict
-
-
 bulk_oxides_dict = json.load(open(os.path.join(f, 'bulk_oxides_20220621.json'), 'r'))
 bulk_oxides_dict = {entry['entry_id']: entry for entry in bulk_oxides_dict}
 
 
-def get_surface_pbx_diagram(queryengine, criteria, increment=500, Ulim=[-1, 3], plot=True, bare_only=True):
+from matplotlib import cm
 
-    entries = list(queryengine.get_slab_entries(criteria).values())
+def get_defect_comp_label(bulk_comp, slab_comp):
+    cbulk = bulk_comp.as_dict()
+    cslab = slab_comp.as_dict()
+    factor = min([cslab[el]/cbulk[el] for el in cslab.keys() if (cslab[el]/cbulk[el]).is_integer()])
+    excess = slab_comp - factor*bulk_comp
+    ex = excess.as_dict()
+    ex = {el: ex[el]/factor for el in ex.keys()}
+    defect_comp = ''
+    for el in cbulk:
+        defect_comp+=el
+        if el in ex.keys():
+            defect_comp += '$_{%s+%.2f}$' %(int(cbulk[el]), ex[el])
+        else:
+            defect_comp += '$_{%s}$' %(int(cbulk[el]))
+    return defect_comp
+
+
+def get_surface_pbx(entries, bulk_comp, queryengine, Ulim=[-1, 3], plot=True, bare_only=True, T=298.15, savefile='', label_rxn=True):
+    
+    muH2O = -14.231 # DFT energy per formula of H2O from MP 
+    muH2 = -6.7714828 # DFT energy per formula of H2 from MP 
+    EO = -4.946243415 # DFT energy per atom of O2 from MP
+
     if bare_only:
         entries = [entry for entry in entries if 'adslab-' not in entry.entry_id]
     ref_entries = {}
@@ -448,91 +391,184 @@ def get_surface_pbx_diagram(queryengine, criteria, increment=500, Ulim=[-1, 3], 
                                                                MAPIKEY=queryengine.MAPIKEY)
         preset_slabentry_se(entry, bulk_entry, MAPIKEY=queryengine.MAPIKEY,
                             ref_entries=ref_entries[entry.data['mpid']])
+
+    # solve all combinations of equations and collect all equations
+    muO = muH2O - muH2 - 2*queryengine.get_e_transfer_corr(T, U=Symbol('U'), pH=Symbol('pH'))
+
+    solved_sys_of_eqns = []
+    for two_entries in itertools.combinations(entries, 2):
+        entry1, entry2 = two_entries
+        eqn1 = float(entry1.preset_surface_energy) if type(entry1.preset_surface_energy).__name__ == 'float'\
+        else entry1.preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
+        eqn2 = float(entry2.preset_surface_energy) if type(entry2.preset_surface_energy).__name__ == 'float'\
+        else entry2.preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
         
-    slabentry_dict, stable_slabentry_dict = {}, {}
-    for slabentry in entries:
-        mpid = slabentry.data['mpid']
-        if mpid not in slabentry_dict.keys():
-            slabentry_dict[mpid] = {}
-            stable_slabentry_dict[mpid] = {}
-        hkl = slabentry.miller_index
-        if hkl not in slabentry_dict[mpid].keys():
-            slabentry_dict[mpid][hkl] = []
-            stable_slabentry_dict[mpid][hkl] = []
-        slabentry_dict[mpid][hkl].append(slabentry)
+        # get rid of 0 coefficients due to stupid float rounding error
+        if type(eqn1).__name__ == 'float' and type(eqn2).__name__ == 'float':
+            continue
+        eqn_diff = eqn1-eqn2
+        eqn_dict = eqn_diff.as_coefficients_dict()
+        new_eqn_diff = 0
+        for k in eqn_dict.keys():
+            if round(eqn_dict[k], 6) != 0:
+                new_eqn_diff += eqn_dict[k]*k
+        eqn_diff = new_eqn_diff
+
+        u = solve(eqn_diff)
+        if not u:
+            continue
+        u = u[0]
+        solved_sys_of_eqns.append(u)
+        
+    # Sort the equations by the intercept, get a mid point between each pair 
+    # of lines, determine which facets are most stable at those mid points
+    solved_sys_of_eqns = [eqn[Symbol('U')] for eqn in solved_sys_of_eqns]
+    
+    solved_sys_of_eqns = sorted(solved_sys_of_eqns, key=lambda eqn: eqn.as_coefficients_dict()[1])
+    stability_map = {}
+    for i, eqn in enumerate(solved_sys_of_eqns):
+        # what is stable below and above this line?
+        U = eqn.as_coefficients_dict()[1]
+        stable_above, stable_below = None, None
+        e_above, e_below = 100, 100
+        
+        for entry in entries:
+            if type(entry.preset_surface_energy).__name__ == 'float':
+                se = float(entry.preset_surface_energy)
+            else:
+                se = entry.preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
+            nse = se - 0.001 if type(entry.preset_surface_energy).__name__ == 'float' else se.subs({'U': U-0.001, 'pH': 0})
+            if e_below > nse:
+                e_below = nse
+                stable_below = entry
+            nse = se + 0.001 if type(entry.preset_surface_energy).__name__ == 'float' else se.subs({'U': U+0.001, 'pH': 0})
+            if e_above > nse:
+                e_above = nse
+                stable_above = entry
+        stability_map[eqn] = {'above': stable_above, 'below': stable_below}
+    
+    most_stable = []
+    for eqn in stability_map.keys():
+        stab_entries = list(stability_map[eqn].values())
+        if stab_entries[0].entry_id == stab_entries[1].entry_id:
+            continue
+        most_stable.append(eqn)
+        
+    most_stable = sorted(most_stable, key=lambda eqn: eqn.as_coefficients_dict()[1])
+    color_float = np.linspace(0, 1, len(most_stable)+1)
+    most_stable_map = {}
+    for i, eqn in enumerate(most_stable):
+        stab_entries = list(stability_map[eqn].values())
+        if plot:
+            plt.plot([0,14], [eqn.subs({'pH': 0}), eqn.subs({'pH': 14})], 'k--')
+        label_above = stability_map[eqn]['above'].composition.formula.replace(' ', '')
+        label_below = stability_map[eqn]['below'].composition.formula.replace(' ', '')
+        
+        most_stable_map[eqn] = {'above': stability_map[eqn]['above'], 
+                                'below': stability_map[eqn]['below']}
+                
+        if plot:
+            l = get_defect_comp_label(bulk_comp, stability_map[eqn]['below'].composition)
+            plt.fill_between([0,100], [float(eqn.subs({'pH': 0})), float(eqn.subs({'pH': 100}))], [-100, 0], 
+                             color=cm.rainbow(color_float[i]), zorder=-1*float(eqn.as_coefficients_dict()[1]), label=l)
+
+    if plot:
+        l = get_defect_comp_label(bulk_comp, stability_map[eqn]['above'].composition)
+        plt.fill_between([0,100], [100, 100], [-100, 0], 
+                         color=cm.rainbow(color_float[len(most_stable)]), zorder=-1000, label=l)
+
+        slope = queryengine.get_e_transfer_corr(T, U=0, pH=1)
+        plt.plot([0, 14], [slope*0+1.23, slope*14+1.23], 'b--')
+        if label_rxn:
+            plt.annotate(r'$2H_2O \leftrightarrow O_2 + 4H^+ + 4e^-$', xy=[7, 0.65], 
+                         rotation=np.rad2deg(np.arcsin(slope)), color='b')
+
+        plt.legend()
+        plt.xlim([0,14])
+        plt.ylim(Ulim)
+        plt.xlabel('pH', fontsize=15)
+        plt.ylabel('Applied Potential (V)', fontsize=15)
+        if savefile:
+            plt.savefig(savefile)
+        plt.show()
+    
+    return most_stable_map
+
+def get_pourbaix_overpotential(queryengine, Gads_dict, stability_map, savefile=None, 
+                               T=298.15, increment=100, Ulim=[-1, 3], pathway='All'):
     
     muH2O = -14.231 # DFT energy per formula of H2O from MP 
     muH2 = -6.7714828 # DFT energy per formula of H2 from MP 
     EO = -4.946243415 # DFT energy per atom of O2 from MP
-    for mpid in slabentry_dict.keys():
-        
-        bulk_formula = queryengine.surface_properties.distinct('bulk_reduced_formula',
-                                                               {'entry_id': mpid})[0]
+    muO = muH2O - muH2 - 2*queryengine.get_e_transfer_corr(T, U=Symbol('U'), pH=Symbol('pH'))
 
-        for hkl in slabentry_dict[mpid].keys():
-
-            pH_range = np.linspace(0, 14, increment)
-            U_range = np.linspace(Ulim[0], Ulim[1], increment)
-            # print(mpid, hkl, len(slabentry_dict[mpid][hkl]))
+    # Get se dict
+    se_dict = {}
+    for d in stability_map.values():
+        se_dict[d['above'].entry_id] = float(d['above'].preset_surface_energy) \
+        if 'float' in type(d['above'].preset_surface_energy).__name__  else \
+        d['above'].preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
+        se_dict[d['below'].entry_id] = float(d['below'].preset_surface_energy) \
+        if 'float' in type(d['below'].preset_surface_energy).__name__  else \
+        d['below'].preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']})
+    
+    # get the grid size for the Pourbaix diagram
+    pH_range = np.linspace(0, 14, increment)
+    U_range = np.linspace(Ulim[0], Ulim[1], increment)
+    
+    dynamic_activity = []
+    for i, pH in enumerate(pH_range):
+        new_row = []
+        for ii, U in enumerate(U_range):
+                        
+            # determine the most stable facet at this pH and U
+            ses = []
+            for se in se_dict.values():
+                e = float(se) if 'float' in type(se).__name__ else se.subs({'pH': pH, 'U': U})
+                ses.append(e)
+                
+            slab_rid = list(se_dict.keys())[ses.index(min(ses))]
+            eads_dict = Gads_dict[slab_rid]
+            GadsO = float(eads_dict['O'][1]) if 'float' in type(eads_dict['O'][1]).__name__ \
+            else eads_dict['O'][1].subs({'pH': pH, 'U': U})
+            GadsOH = float(eads_dict['OH'][1]) if 'float' in type(eads_dict['OH'][1]).__name__ \
+            else eads_dict['OH'][1].subs({'pH': pH, 'U': U})
+            GadsOOH = float(eads_dict['OOH'][1]) if 'float' in type(eads_dict['OOH'][1]).__name__ \
+            else eads_dict['OOH'][1].subs({'pH': pH, 'U': U})
             
-            stable_facet = []
-            for i, pH in enumerate(pH_range):
-                new_row = []
-                for ii, U in enumerate(U_range):
+            if pathway=='All':
+                overpotential = float(max([GadsOH, GadsO-GadsOH, GadsOOH-GadsO, 
+                                           1.23*4-GadsOOH]))
+            elif pathway == '1st_half':
+                overpotential = float(max([GadsOH, GadsO-GadsOH]))
+            elif pathway == '2nd_half':
+                overpotential = float(max([GadsOOH-GadsO, 1.23*4-GadsOOH]))
 
-                    """
-                    The surface energy of nonstoichometric oxide surface as a fucntion of pH and O 
-                    (e.g. surface grand potential) is used to determine the Pourbaix diagram. It is 
-                    given as γ = (E^* - Σnμ_i)/2A. Since we're building a Pourbaix diagram on a per 
-                    facet basis, we can just ignore 2A for now since all facets we are considering 
-                    have the same surface area. μ_i can be rewritten as the chemical potential of 
-                    oxygen only, e.g. μ_O. Here, we assume the following chemical reaction takes 
-                    place under an aqueous condition: μ_H2O(l) --> 2μ_H + μ_O. As such, 
-                    μ_O = μ_H2O(l) - 2μ_H. We assume μ_H2O(l) = E_H2O (Gibbs free energy of H2O in 
-                    a box). We then assume charge transfer when a H atom is in the reaction, e.g.:
-                    μ_H = μ_H^+ + μ_e- = 1/2G_H2 - eU + kBTlna^+ via the Nernst Equation. Ergo
-                    μ_O = G_H2O(l) - G_H2 + 2eU - 2kBTlna^+. Since we don't have G, we'll just add
-                    a correction value for converting between E and G: 
-                    μ_O = E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr
-                    Lastly, we can also reference μ_O to the energy of O2 (keep it simple and 
-                    reference to 1/2E_O2 for now) which gives us Δμ_O = μ_O-1/2EO_2 or:
-                    Δμ_O = E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr - 1/2E_O2
-                    As such, the surface grand potential can be written as:
-                    2Aγ(U, pH) = E^* - n(E_H2O(l) - E_H2 + 2eU - 2kBTlna^+ + ΔG_corr - 1/2E_O2) 
-                    where n determines the number of excess/defficient species relative to oxygen.
-                    """
-                    muO = muH2O - muH2 - 2*queryengine.get_e_transfer_corr(298.15, U=U, pH=pH)
-                    DG = [slabentry.preset_surface_energy.subs({'delu_O': muO-EO + queryengine.Gcorr['O']}) \
-                          if type(slabentry.preset_surface_energy).__name__ != 'float' \
-                          else slabentry.preset_surface_energy \
-                          for slabentry in slabentry_dict[mpid][hkl]]
-                    a = DG.index(min(DG))
-                    entry = slabentry_dict[mpid][hkl][a]
-                    if entry not in stable_slabentry_dict[mpid][hkl]:
-                        stable_slabentry_dict[mpid][hkl].append(entry)
-                    new_row.append(a)
-                    
-                stable_facet.append(new_row)
-                
-            if plot:
-                
-                pH_range, U_range = np.meshgrid(pH_range, U_range)
-                stable_facet = np.array(stable_facet)
-                plt.xlim([0,14])
-                plt.ylim(Ulim)
-                plt.contourf(pH_range, U_range, stable_facet, cmap='rainbow',
-                             vmin=-np.abs(stable_facet).max(),
-                             vmax=np.abs(stable_facet).max())
+            new_row.append(overpotential)
+        dynamic_activity.append(new_row)
+        
+    pH_range, U_range = np.meshgrid(pH_range, U_range)
+    plt.pcolormesh(pH_range, U_range, np.array(dynamic_activity).T, vmin=0, 
+                   vmax=6, cmap='rainbow')
+    
+    for eqn in stability_map.keys():
+        plt.plot([0,14], [eqn.subs({'pH': 0}), eqn.subs({'pH': 14})], 'k--')
+        
+    slope = queryengine.get_e_transfer_corr(T, U=0, pH=1)
+    plt.plot([0, 14], [slope*0+1.23, slope*14+1.23], 'b--')
+    plt.annotate(r'$2H_2O \leftrightarrow O_2 + 4H^+ + 4e^-$', xy=[7, 0.65], 
+                 rotation=np.rad2deg(np.arcsin(slope)), color='b')
 
-                plt.title('%s %s %s' %(bulk_formula, mpid, tuple(hkl)))
-                plt.xlabel(r'pH', fontsize=20)
-                plt.ylabel(r'U (V)', fontsize=20)
+    plt.xlim([0,14])
+    plt.ylim(Ulim)
+    plt.xlabel('pH', fontsize=15)
+    plt.ylabel('Applied Potential (V)', fontsize=15)
+    cbar = plt.colorbar()
+    cbar.set_label(r'$\Delta G^{RDS}_{rxn}$ (eV)', fontsize=20)
 
-                plt.plot([0,14], [1.2,1.2], 'k--')
-                plt.plot([0,14], [2,2], 'k--')
-
-                plt.savefig('%s_%s_%s%s%s.pdf' %(bulk_formula, mpid, hkl[0],hkl[1],hkl[2]))
-                plt.show()
-                plt.close()
-
-    return stable_slabentry_dict
+    if savefile:
+        plt.savefig(savefile)
+    plt.show()
+    plt.close()
+    
+    return dynamic_activity
