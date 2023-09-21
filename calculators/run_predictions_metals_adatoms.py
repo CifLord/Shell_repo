70ba41b3910ee64e4e7652cbf78347690c5a800a
CifLord <rit634989@gmail.com>
2023-09-21 00:07:31 -0500

    added new lmdb gneerator for multimetallics

---
+++ calculators/run_predictions_metals_adatoms.py
@@ -0,0 +1,119 @@
+import sys, random, os, json, threading, lmdb, pickle, torch, argparse, time
+import vaspjob
+f = vaspjob.__file__
+repo_dir = f.replace(os.path.join(f.split('/')[-2], f.split('/')[-1]), '')
+sys.path.append(repo_dir)
+
+from prediction_tools import MyThread
+from ocpmodels.datasets import LmdbDataset
+import logging
+from structure_generation.bare_slabs import slab_generator
+from structure_generation.lmdb_generator import generate_lmdb, lmdb_size
+from structure_generation.oxide_adsorption import surface_adsorption, ads_dict
+from structure_generation.lmdb_generator import convert_atoms_data
+from tqdm import tqdm
+
+def read_options():
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--input_lmdb", dest="input_lmdb", type=str, 
+                        help="Name of lmdb file to write slab/adslabs to (prior to predictions)")
+    parser.add_argument("-j", "--json_list", dest="json_path", type=str,default=None, 
+                        help="Name of json file to of mp-ids")
+    parser.add_argument("-m", "--mpids", dest="list_of_mpids", type=str, default=None,
+                        help="List of mpids to run slab/adslab predictions on")
+    parser.add_argument("-d", "--nthreads", dest="number_of_threads", type=int, default=4,
+                        help="Number of threads to distribute predictions to")
+    parser.add_argument("-w", "--mmi", dest="mmi", type=int, default=1,
+                        help="Max miller index")
+    parser.add_argument("-s", "--slab_size", dest="slab_size", type=float, default=12.5,
+                        help="Slab size")
+    parser.add_argument("-v", "--vac_size", dest="vac_size", type=float, default=12.5,
+                        help="Vacuum size")
+    parser.add_argument("-k", "--mapikey", dest="MAPIKEY", type=str, 
+                        default='11nq6MypZgP6PxZwugyPGW9w6UFVF5Ja',
+                        help="Materials Project API KEY")
+    parser.add_argument("-b", "--debug", dest="debug", type=str, default=False, 
+                        help="Run in debug mode, ie don't run the ASE calculator but do everything else")
+    parser.add_argument("-g", "--gpus", dest="gpus", type=int, default=1, 
+                        help="Number of GPUs available")
+    parser.add_argument("-r", "--run_predictions", dest="run_predictions", type=bool, default=True, 
+                        help="Whether or not to run predictions")
+    parser.add_argument("-a", "--remove_ads", dest="remove_ads", type=str, default=None, 
+                        help="Adsorbate to omit")
+    parser.add_argument("-n", "--max_natoms", dest="max_natoms", type=str, default=200, 
+                        help="Max number of atoms in surface")
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__=="__main__":
+    
+    initT = time.time()
+    
+    args = read_options()  
+    if args.list_of_mpids is None:
+        if args.json_path is None:
+            raise TypeError("Need input mp-ids,either in json or list")
+        else:        
+            with open(args.json_path) as f:
+                mpid_list=json.load(f)
+    else:    
+        mpid_list = args.list_of_mpids.split(' ')
+        
+    if args.remove_ads:
+        del ads_dict[args.remove_ads]
+
+    p=0
+    log_fname=str(args.input_lmdb).replace('.lmdb','.log')
+    logging.basicConfig(filename=log_fname, level=logging.INFO, 
+                        format='%(asctime)s %(levelname)s: %(message)s')
+    os.makedirs('prediction',exist_ok=True)
+    for mpid in tqdm(mpid_list):
+        all_atoms_slabs = []        
+        # Generate all bare slabs
+        slab_atoms = slab_generator(mpid, args.mmi, args.slab_size, args.vac_size, 
+                                    MAPIKEY=args.MAPIKEY, height_tol=2, min_lw=8, tol=0.1, 
+                                    functional='GemNet-OC', count_undercoordination=False)
+        if len(slab_atoms)==0:
+            continue
+        slab_atoms = [a for a in slab_atoms if len(a) < args.max_natoms]
+        all_atoms_slabs.extend(slab_atoms)        
+        # Generate all adslabs
+        for slab in slab_atoms:
+            adslabs = surface_adsorption(convert_atoms_data(slab), ads_dict=ads_dict)
+            all_atoms_slabs.extend(adslabs)
+        input_pathname=args.input_lmdb.rstrip('.lmdb')+'{:04d}'.format(p)+'.lmdb' 
+        logging.info('Total number of predictions: %s' %(len(all_atoms_slabs)))       
+        #print('Total number of predictions: %s' %(len(all_atoms_slabs)))
+        
+        if lmdb_size(input_pathname) >=10000:
+            p+=1
+            input_pathname=args.input_lmdb.rstrip('.lmdb')+'{:04d}'.format(p)+'.lmdb'                  
+        generate_lmdb(all_atoms_slabs, input_pathname)
+        #print('finished slab generation: %s' %(mpid))
+        logging.info('finished slab generation: %s' %(mpid)) 
+        
+    if p==0:
+        input_lmdbs=[args.input_lmdb.rstrip('.lmdb')+'{:04d}'.format(p)+'.lmdb']
+    else:        
+        input_lmdbs=[args.input_lmdb.rstrip('.lmdb')+'{:04d}'.format(p)+'.lmdb' for p in range(p)]   
+    logging.info('finished all slab generation: %s' %(mpid)) 
+    
+    if args.run_predictions:
+        for i in input_lmdbs:
+            input_lmdb = LmdbDataset({'src': i})
+            output_lmdb = i.rstrip('.lmdb')+'_ads.lmdb'
+            # equally distribute dataset to multiple threads
+            for j in range(args.number_of_threads): 
+                lp = range(int(len(input_lmdb)/args.number_of_threads)*j, 
+                           int(len(input_lmdb)/args.number_of_threads)*(1+j))
+                thread = MyThread([input_lmdb[ii] for ii in lp], output_lmdb, 
+                                  args.gpus, debug=args.debug)
+                thread.start()
+                sys.stdout = open(os.devnull, "w")
+
+        logging.info('Finished all OC22 predictions in %s' %(time.time() - initT))
---
+++ structure_generation/metal_adatom.py
@@ -0,0 +1,262 @@
+from pymatgen.io.ase import AseAtomsAdaptor
+from pymatgen.core.surface import Slab
+from pymatgen.core.structure import Molecule
+from pymatgen.util.coord import all_distances, pbc_shortest_vectors
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+from pymatgen.core.periodic_table import Element
+from pymatgen.analysis.structure_matcher import StructureMatcher
+from mp_api.client import MPRester
+from database.generate_metadata import write_metadata_json
+from structure_generation.MXide_adsorption import MXideAdsorbateGenerator
+
+from ase.constraints import FixAtoms
+from ase import Atoms
+
+import numpy as np
+import json, copy
+
+
+"""
+Metadata and additional API information for adsorbed slabs:
+    entry_id (str): Materials Project ID of the corresponding bulk
+    database (str): Materials database where the bulk was obtained from
+    adsorbate (str): Name_of_adsorbate
+    nads (int): Number of adsorbate molecules
+    bulk_rid (str): 20 digit random ID of the corresponding bulk e.g. 
+        bulk-h4qfsgj3432kl113DVpD
+    slab_rid (str): 20 digit random ID of the corresponding bare slab
+        e.g. slab-t43frgfgy5lFh3f3CVGD
+    ads_rid (str): 20 digit random ID of the corresponding gas phase 
+        reference of the adsorbate e.g. ads-a03Wlk2205GFXqu95ATs
+    rid (str): 20 digit random ID of the adsorbed slab e.g. 
+        adslab-zx668jolRrGdhqwWqA47
+    miller_index (tuple): Miller index of the facet 
+    bulk_formula (str): Reduced formula of the corresponding bulk 
+    bulk_composition (dict): Composition of the bulk with elements 
+        as keys and the number of atoms as values 
+    bulk_chemsys (str): Elemental components of the bulk e.g. for 
+        LiFe(PO4), the chemsys is 'Li-Fe-P-O'
+    pmg_slab (dict): Pymatgen slab object as a dictionary. Contains useful 
+        information about miller index, scale factor, bulk wyckoff positions, etc. 
+    calc_type (str): What kidn of system is this calculation for 
+        ('bare_slab', 'adsorbed_slab', 'adsorbate_in_a_box', 'bulk') 
+    func (str): The DFT functional used or the method used to obtain the 
+        energy/relaxation data (Beef-vdW, PBE, PBEsol, rPBE, GemNet-OC (for ML) etc...)
+"""
+
+from database import generate_metadata
+f = generate_metadata.__file__
+bulk_oxides_20220621 = json.load(open(f.replace(f.split('/')[-1], 'bulk_oxides_20220621.json'), 'rb'))
+bulk_oxides_dict = {entry['entry_id']: ComputedStructureEntry.from_dict(entry) \
+                    for entry in bulk_oxides_20220621}
+
+ads_dict = {'O': [Molecule(["O"], [[0,0,0]])], 'H': [Molecule(["H"], [[0,0,0]])], 
+            'N': [Molecule(["N"], [[0,0,0]])], 'C': [Molecule(["C"], [[0,0,0]])]}
+
+MAPIKEY='HO6BA47aEuOPR8Puc0qshrc6G9596FNa' #Liqiang Key
+
+def strip_entry(input_string):
+    # when get the entry_id in lmdb, we may get the str like mp-0000-GGA
+    # this method strip the str to mp-0000
+    second_hyphen_index = input_string.find('-', input_string.find('-') + 1)
+    if second_hyphen_index != -1:
+        return (input_string[:second_hyphen_index])
+    else:
+        return (input_string)
+
+def surface_adsorption(slab_data, functional='GemNet-OC', coverage_list=[1], 
+                       MAPIKEY=MAPIKEY, ads_dict=ads_dict):
+    """
+    Gets all adsorbed slab for a slab. Will always return 6 adslabs, 
+        1 O* saturated slab and 5 OH saturated slabs. 4 of the OH  
+        adslabs will have all OH molecules pointing in one of the 4 
+        cardinal directions. 1 OH adslab will place OH* in such a way as 
+        to minimize the O-H bondlengths with all O* and surface O atoms.
+    """
+    
+    # get bulk entry
+    entry_id=slab_data.entry_id
+    entry_id=strip_entry(entry_id)
+    if entry_id not in bulk_oxides_dict.keys():
+        mprester = MPRester(MAPIKEY) if MAPIKEY else MPRester()
+        bulk_entry = mprester.get_entry_by_material_id(entry_id, inc_structure=True,
+                                                       conventional_unit_cell=True)
+        bulk_entry=bulk_entry[0] # new key need to index
+    else:
+        bulk_entry = bulk_oxides_dict[entry_id]
+    
+    # get pmg slab
+    init_slab = Slab.from_dict(json.loads(slab_data.init_pmg_slab))
+    
+    # I am assuming the atomic_numbers, pos, and 
+    # cell are corresponding to the relaxed slab?
+    # convert relaxed slab to Slab
+    atoms = Atoms(slab_data.atomic_numbers,
+                  positions=slab_data.pos,# Tentatively, changed to pox---slab_data.pos_relaxed
+                  tags=slab_data.tags,
+                  cell=slab_data.cell.squeeze(), pbc=True)
+    relaxed_slab = AseAtomsAdaptor.get_structure(atoms)
+    relaxed_slab = Slab(relaxed_slab.lattice, relaxed_slab.species,
+                        relaxed_slab.frac_coords, init_slab.miller_index,
+                        init_slab.oriented_unit_cell, init_slab.shift, 
+                        init_slab.scale_factor, site_properties=init_slab.site_properties)
+
+    mxidegen = MXideAdsorbateGenerator(init_slab, positions=['MX_adsites'], 
+                                       selective_dynamics=True, repeat=[1,1,1])
+    
+    props = mxidegen.slab.site_properties
+    for k in props.keys():
+        if k not in relaxed_slab.site_properties.keys():
+            relaxed_slab.add_site_property(k, props[k])
+    
+    all_adslabs = []
+    relaxed_adslabs = {}
+    for adsname in ads_dict.keys():
+        relaxed_adslabs[adsname] = []
+        if coverage_list == 'saturated' and adsname == 'OOH':
+            continue
+        for mol in ads_dict[adsname]:
+            relslab = relaxed_slab.copy()
+            if coverage_list == 'saturated':
+                adslabs = adsorb_saturate(relslab, mxidegen, mol)
+            else:
+                adslabs = adsorb_one(relslab, mxidegen, mol)
+                
+            for adslab in adslabs:
+                adslab.replace_species({Element('N'): Element('O')})
+                setattr(adslab, 'adsorbate', adsname)
+            relaxed_adslabs[adsname].extend(adslabs)
+
+    if coverage_list == 'saturated':
+        OHstar = max_OH_interaction_adsorption(relslab, mxidegen)
+        setattr(OHstar, 'adsorbate', 'OH')
+        relaxed_adslabs['OH'].append(OHstar)
+                
+    sm = StructureMatcher()
+    relaxed_adslabs_list = []
+    for ads in relaxed_adslabs.keys():
+        relaxed_adslabs_list.extend([g[0] for g in sm.group_structures(relaxed_adslabs[ads])])
+    
+    # Build list of Atoms objects
+    adslab_atoms = []
+    for adslab in relaxed_adslabs_list:
+        
+        # name adsorbates
+        if adslab.adsorbate == 'O':
+            nads = len([site for site in adslab if site.surface_properties == 'adsorbate'])
+        elif adslab.adsorbate == 'OH':
+            nads = len([site for site in adslab if site.surface_properties == 'adsorbate'])/2
+        elif adslab.adsorbate == 'OOH':
+            nads = len([site for site in adslab if site.surface_properties == 'adsorbate'])/3
+
+        new_tags = [] 
+        for site in adslab:
+            if site.tag == None:
+                new_tags.append(2)
+            elif site.frac_coords[2] < 0.5:
+                new_tags.append(0)
+            else:
+                new_tags.append(site.tag)
+        adslab.add_site_property('tag', new_tags)
+
+        # get metadata 
+        database = 'MP' if 'mp-' in bulk_entry.entry_id else None
+        metadata = write_metadata_json(adslab, 'adsorbed_slab', bulk_entry, 
+                                       name_of_adsorbate=adslab.adsorbate,
+                                       database=database, slab_rid=slab_data.rid,
+                                       functional=functional, additional_data={'nads': nads})
+        
+        # ASE Atoms object format, set up selective dynamics so  
+        # that only the top surface and adsorbate relax this time
+        atoms = AseAtomsAdaptor.get_atoms(adslab, **{'info': metadata})
+        atoms.set_tags(new_tags)
+        atoms.set_constraint(FixAtoms([i for i, site in enumerate(adslab) if site.tag == 0]))
+        adslab_atoms.append(atoms)
+    
+    return adslab_atoms
+
+def adsorb_one(slab, mxidegen, adsorbate):
+
+    transformed_ads_list = mxidegen.get_transformed_molecule_MXides\
+    (adsorbate, [np.deg2rad(deg) for deg in np.linspace(0, 360-120, 3)])
+
+    all_adslabs = []
+    for coord in mxidegen.MX_adsites:
+        for i, mol in enumerate(transformed_ads_list):
+            adslab = slab.copy()
+            for site in mol:
+                adslab.append(site.species, coord+site.coords, coords_are_cartesian=True,
+                              properties={'original_index': None, 'supercell': None, 
+                                          'selective_dynamics': [True, True, True], 
+                                          'bulk_equivalent': None, 'site_type': None, 'tag': 2, 
+                                          'surface_properties': 'adsorbate', 'bulk_wyckoff': None})
+            all_adslabs.append(adslab)
+
+    return all_adslabs
+
+def adsorb_saturate(slab, mxidegen, adsorbate):
+
+    transformed_ads_list = mxidegen.get_transformed_molecule_MXides\
+    (adsorbate, [np.deg2rad(deg) for deg in np.linspace(0, 360, 4)])
+
+    all_adslabs = []
+    for i, mol in enumerate(transformed_ads_list):
+        adslab = slab.copy()
+        for coord in mxidegen.MX_adsites:
+            for site in mol:
+                adslab.append(site.species, coord+site.coords, coords_are_cartesian=True, 
+                              properties={'original_index': None, 'supercell': None, 
+                                          'selective_dynamics': [True, True, True], 
+                                          'bulk_equivalent': None, 'site_type': None, 'tag': 2, 
+                                          'surface_properties': 'adsorbate', 'bulk_wyckoff': None})
+        all_adslabs.append(adslab)
+
+    return all_adslabs
+
+def max_OH_interaction_adsorption(s, mxidegen, incr=100):
+    """
+    Algorithm to saturate a surface with OH by rotating all OH 
+        molecules in such a way to minimize H-O bondlengths 
+        between H* and all surface O and O* sites. This minimization 
+        will hopefully get us a relatively stable config for adsorption.
+    """
+    
+    # Get all surface O sites and potential O* sites
+    surf_Osites = copy.copy(mxidegen.MX_adsites)
+    for site in mxidegen.slab:
+        if all([site.surface_properties == 'surface', 
+                site.frac_coords[-1] > 0.5, site.species_string == 'O']):
+            surf_Osites.append(site.coords)
+
+    transformed_ads_list = mxidegen.get_transformed_molecule_MXides\
+    (OH, [np.deg2rad(deg) for deg in np.linspace(0, 360, incr)])
+
+    satslab = s.copy()
+    for coord in mxidegen.MX_adsites:
+        all_OH_ave_dists = []
+        
+        for i, mol in enumerate(transformed_ads_list):
+            slab = mxidegen.slab.copy()
+            for site in mol:
+                slab.append(site.species, coord+site.coords, coords_are_cartesian=True)
+
+            shortest_vect = pbc_shortest_vectors(slab.lattice, slab[-1].frac_coords, 
+                                                 [slab.lattice.get_fractional_coords(c) \
+                                                  for c in surf_Osites])
+            all_OH_dists = []
+            for i, c in enumerate(surf_Osites):
+                # Get all distances between the H site of the adsorbate
+                # and its surrrounding O sites at the surface
+                all_OH_dists.append(all_distances([slab[-1].coords+shortest_vect[0][i]],
+                                                  [slab[-1].coords])[0][0])
+            all_OH_ave_dists.append(np.mean(all_OH_dists))
+
+        all_OH_ave_dists, transformed_ads_list = zip(*sorted(zip(all_OH_ave_dists, 
+                                                                 transformed_ads_list)))
+
+        for site in transformed_ads_list[0]:
+            satslab.append(site.species, site.coords+coord, coords_are_cartesian=True, 
+                           properties={'tag': 2, 'surface_properties': 'adsorbate',
+                                       'selective_dynamics': [True, True, True]})
+
+    return satslab
